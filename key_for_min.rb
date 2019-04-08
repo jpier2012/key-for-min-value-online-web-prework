37b1116cab72636ed885d4226005cfc48c5c1bdb
James Pier <jpier2012@gmail.com>
2019-04-08 17:02:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,8 @@
 
 def key_for_min_value(name_hash)
   min_value = name_hash
+  min_name = ""
   name_hash.each {|name, value|
-    min_name = name
     min_value = value
     if value < x
       min_name = name
