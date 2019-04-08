3ec1d5ca823958bca6aed456bb18004478117386
James Pier <jpier2012@gmail.com>
2019-04-08 17:07:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,8 @@ def key_for_min_value(name_hash)
   min_value = 0
   name_hash.each {|name, value|
     if min_value < value
-    end
+      min_value = value
+      min_name = name
   break
   }
 end
